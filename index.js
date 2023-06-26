933cfd2a656dd94950ce177a0bec9b5afd3375c4
amirticha <allagui_fawzi@outlook.fr>
2023-06-26 22:24:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+const dotenv = require('dotenv');
+const nodemailer = require("nodemailer");
+const Mailgen = require('mailgen');
+
+dotenv.config();
+const html = `<h1>nodemailer mail </h1>`;
+
+let mailGenerator = new Mailgen({
+    theme:"default",
+    product:{
+        name:"Mailgen",
+        link:'https://mailgen.js/'
+    }
+})
+let response = {
+    body: {
+        name:"amira",
+        intro:"your bill has arrived",
+        table: {
+            data:[
+                {
+                item : "Nodemailer Stack Book",
+                description : "A backend application Stack Book",
+                price : "$100",
+                }
+            ]
+        },
+        outro :"Looking forward to do more business"
+    }
+}
+
+let mail = mailGenerator.generate(response)
+// console.log(mail);
+
+
+async function main(html_text) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    // service: "gmail",
+    port: 465,
+    auth: {
+      user: process.env.GMAIL_ACCOUNT,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: "medfawziallagui@gmail.com",
+    to: "allagui_amira@yahoo.com",
+    subject: "Subject of the email",
+    html: html_text,
+  });
+
+  console.log("message sent", info);
+}
+
+main(mail).catch(e => console.log(e));
+
+
+
+
+
+
+
+
+
+
+// user: "medfawziallagui@gmail.com",
+// pass: "Aa05121993",
